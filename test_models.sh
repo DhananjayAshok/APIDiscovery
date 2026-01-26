02cf31775850f9f2016d2a6724c38dd8adb33c7d
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2026-01-25 16:25:09 -0800

    Why

--- test_models.sh
+++ test_models.sh
@@ -4,6 +4,6 @@ datasets=("humaneval" "cruxeval" "mbpp")
 for dataset_name in "${datasets[@]}"; do
     for model_name in "${models[@]}"; do
         echo "Testing model: $model_name on dataset: $dataset_name"
-        python eval.py --dataset_name "$dataset_name" --model_name "$model_name" --override_eval
+        python eval.py --dataset_name "$dataset_name" --model_name "$model_name"
     done
 done
