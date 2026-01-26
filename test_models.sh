4a9d629fe48c2955a642a584f0a74368d1f07991
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2026-01-25 16:21:34 -0800

    idk

--- test_models.sh
+++ test_models.sh
@@ -1,8 +1,8 @@
 models=("meta-llama/Meta-Llama-3-8B-Instruct" "gpt-4o-mini")
 datasets=("humaneval" "cruxeval" "mbpp")
 
-for model_name in "${models[@]}"; do
-    for dataset_name in "${datasets[@]}"; do
+for dataset_name in "${datasets[@]}"; do
+    for model_name in "${models[@]}"; do
         echo "Testing model: $model_name on dataset: $dataset_name"
         python eval.py --dataset_name "$dataset_name" --model_name "$model_name" --override_eval
     done
