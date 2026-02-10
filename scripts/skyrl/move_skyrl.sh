51da61be7a97fec4a6f1e8ef632a439dc9fd253e
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2026-02-09 19:06:35 -0800

    AAAH

--- scripts/skyrl/move_skyrl.sh
+++ scripts/skyrl/move_skyrl.sh
@@ -60,10 +60,10 @@ source configs/config.env
 
 # copy the contents of configs/config.env and prepend it to the front of scripts/skyrl/run_rl.sh
 { cat "configs/config.env"; cat scripts/skyrl/run_rl.sh; } > temp.txt && mv temp.txt scripts/skyrl/run_rl.sh
-{ cat "export DATA_DIR=$storage_dir/data/parquets/${ARGS["d"]}"; cat scripts/skyrl/run_rl.sh; } > temp.txt && mv temp.txt scripts/skyrl/run_rl.sh
-{ cat "export trainer_policy_model=${ARGS["m"]}"; cat scripts/skyrl/run_rl.sh; } > temp.txt && mv temp.txt scripts/skyrl/run_rl.sh
-{ cat "export run_name=${ARGS["r"]}"; cat scripts/skyrl/run_rl.sh; } > temp.txt && mv temp.txt scripts/skyrl/run_rl.sh
-{ cat "export NUM_GPUS=${ARGS["n"]}"; cat scripts/skyrl/run_rl.sh; } > temp.txt && mv temp.txt scripts/skyrl/run_rl.sh
+{ echo "export DATA_DIR=$storage_dir/data/parquets/${ARGS["d"]}"; cat scripts/skyrl/run_rl.sh; } > temp.txt && mv temp.txt scripts/skyrl/run_rl.sh
+{  echo "export trainer_policy_model=${ARGS["m"]}"; cat scripts/skyrl/run_rl.sh; } > temp.txt && mv temp.txt scripts/skyrl/run_rl.sh
+{ echo "export run_name=${ARGS["r"]}"; cat scripts/skyrl/run_rl.sh; } > temp.txt && mv temp.txt scripts/skyrl/run_rl.sh
+{ echo "export NUM_GPUS=${ARGS["n"]}"; cat scripts/skyrl/run_rl.sh; } > temp.txt && mv temp.txt scripts/skyrl/run_rl.sh
 
 
 mkdir -p SkyRL/skyrl-train/examples/function_discovery/
